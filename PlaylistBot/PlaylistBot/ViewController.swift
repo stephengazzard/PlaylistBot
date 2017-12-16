bc8d1fa1c03d425da054701bdd39620eb28f4780
stephengazzard <stephen.gazzard@gmail.com>
2017-12-16 15:02:48 -0700

    init project

---
+++ PlaylistBot/PlaylistBot/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PlaylistBot
+//
+//  Created by Stephen Gazzard on 2017-12-16.
+//  Copyright © 2017 Robots and Pencils Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
