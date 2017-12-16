6c5da98ef10fe3be7063b38c9eebb6557e8c63a4
stephengazzard <stephen.gazzard@gmail.com>
2017-12-16 15:11:56 -0700

    Add PlaylistBuilder Framework target

---
+++ PlaylistBot/PlaylistBuilder/PlaylistBuilder.h
@@ -0,0 +1,19 @@
+//
+//  PlaylistBuilder.h
+//  PlaylistBuilder
+//
+//  Created by Stephen Gazzard on 2017-12-16.
+//  Copyright © 2017 Robots and Pencils Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PlaylistBuilder.
+FOUNDATION_EXPORT double PlaylistBuilderVersionNumber;
+
+//! Project version string for PlaylistBuilder.
+FOUNDATION_EXPORT const unsigned char PlaylistBuilderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PlaylistBuilder/PublicHeader.h>
+
+
