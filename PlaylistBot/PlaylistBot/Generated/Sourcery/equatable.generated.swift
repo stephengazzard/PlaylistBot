ce405209b9303fbf6c1c82b47b4f333638167beb
stephengazzard <stephen.gazzard@gmail.com>
2017-12-17 09:48:24 -0700

    Add Sourcery / Swiftlint build steps

---
+++ PlaylistBot/PlaylistBot/Generated/Sourcery/equatable.generated.swift
@@ -0,0 +1,29 @@
+// Generated using Sourcery 0.9.0 — https://github.com/krzysztofzablocki/Sourcery
+// DO NOT EDIT
+
+// swiftlint:disable file_length
+func compareOptionals<T>(lhs: T?, rhs: T?, compare: (_ lhs: T, _ rhs: T) -> Bool) -> Bool {
+    switch (lhs, rhs) {
+    case let (lValue?, rValue?):
+        return compare(lValue, rValue)
+    case (nil, nil):
+        return true
+    default:
+        return false
+    }
+}
+
+func compareArrays<T>(lhs: [T], rhs: [T], compare: (_ lhs: T, _ rhs: T) -> Bool) -> Bool {
+    guard lhs.count == rhs.count else { return false }
+    for (idx, lhsItem) in lhs.enumerated() {
+        guard compare(lhsItem, rhs[idx]) else { return false }
+    }
+
+    return true
+}
+
+
+
+
+
+
